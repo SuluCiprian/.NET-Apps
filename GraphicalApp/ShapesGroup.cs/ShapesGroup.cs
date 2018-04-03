95a487a72ddafaeecff886d156ac9ac7f9e12881
SFTLCRPC039\ciprian.sulu <cipriansulu1@yahoo.com>
2018-04-03 13:34:45 +0300

    refactor

---
+++ GraphicalApp/ShapesGroup.cs/ShapesGroup.cs
@@ -0,0 +1,40 @@
+﻿using GraphicalApp.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphicalApp
+{
+    class ShapesGroup: IShape
+    {
+        private string name;
+        private int identifier;
+        private List<IShape> groupShpes = new List<IShape>();
+
+        public ShapesGroup(string name)
+        {
+            this.name = name;
+        }
+
+        public int Identifier { get => identifier; set => identifier = value; }
+
+        public void AddToGroup(IShape shape)
+        {
+            groupShpes.Add(shape);
+        }
+
+        public double Area()
+        {
+            return 0;
+        }
+
+        public void Draw()
+        {
+            foreach (var shape in groupShpes)
+            {
+                shape.Draw();
+            }
+        }
+
+    }
+}
---
+++ GraphicalApp/ShapesGroup.cs/ShapesGroupPlugin.cs
@@ -0,0 +1,30 @@
+﻿using GraphicalApp.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShapesGroup.cs
+{
+    class ShapesGroupPlugin : IShapePlugin
+    {
+        public string GetName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetRequiredArguments()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IShape GetShape()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetArguments(IDictionary<string, string> args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
